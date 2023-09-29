4a2f3352a52a1319251e0fc536b0cf32af3f6dd3
Milena-Calado <mcsa2@cin.ufpe.br>
2023-09-29 14:28:22 -0300

    Finalizando

--- Presc.py
+++ Presc.py
@@ -1,19 +1,8 @@
 import pgf
 
 gr = pgf.readPGF('/media/mcsa2/6824EAA724EA7788/Users/mcsa2/Documents/dev/NLP-Prescricoes-Medicas/PrescAbsBra.pgf')
-por = gr.languages["PrescConcBra"]
 eng = gr.languages["PrescConcEng"]
-
-print("Exepmplos de frases em Portugues: ")
-print("--------------------------------------")
-print("Aplicar 1 dose de benzetacil 20ml de 8 em 8 horas")
-print("Tomar 1 comprimido de paracetamol 50mg de 12 em 12 horas")
-print("Tomar 2 comprimidos de dipirona 50mg de 8 em 8 horas")
-print("--------------------------------------")
-print("Exepmplos de frases em Inglês: ")    
-print("Take 1 pill in paracetamol 50mg every 12 hours")
-print("Apply 1 injection in profenid 10ml every 12 hours")
-print("--------------------------------------")
+por = gr.languages["PrescConcBra"]
 
 
 while True:
@@ -27,9 +16,9 @@ while True:
      # Verifica se a frase contém palavras em inglês
     if any(word in name.lower() for word in ["take", "pill", "injection"]):
         try:
-            i = por.parse(name)
+            i = eng.parse(name)
             p, e = i.__next__()
-            print("Prescrição Válida: " + por.linearize(e))
+            print("Prescrição Válida: " + eng.linearize(e))
             break
         except pgf.ParseError:
             print("Prescrição Inválida, tente novamente")
