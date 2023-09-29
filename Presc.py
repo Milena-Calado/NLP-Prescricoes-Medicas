d4b5c9a0f2d1f64de8ba3fb5befacd9a850e61a3
Milena-Calado <mcsa2@cin.ufpe.br>
2023-09-29 13:57:31 -0300

    Gerando o pgf

--- Presc.py
+++ Presc.py
@@ -1,18 +1,23 @@
 import pgf
 
-gr = pgf.readPGF('/media/mcsa2/6824EAA724EA7788/Users/mcsa2/Documents/dev/NLP-Prescricoes-Medicas/PrescAbs.pgf')
-eng = gr.languages["PrescConcEng"]
+gr = pgf.readPGF('/media/mcsa2/6824EAA724EA7788/Users/mcsa2/Documents/dev/NLP-Prescricoes-Medicas/PrescAbsBra.pgf')
 por = gr.languages["PrescConcBra"]
+eng = gr.languages["PrescConcEng"]
+
+print("Exepmplos de frases em Portugues: ")
+print("--------------------------------------")
+print("Aplicar 1 dose de benzetacil 20ml de 8 em 8 horas")
+print("Tomar 1 comprimido de paracetamol 50mg de 12 em 12 horas")
+print("Tomar 2 comprimidos de dipirona 50mg de 8 em 8 horas")
+print("--------------------------------------")
+print("Exepmplos de frases em Inglês: ")    
+print("Take 1 pill in paracetamol 50mg every 12 hours")
+print("Apply 1 injection in profenid 10ml every 12 hours")
+print("--------------------------------------")
+
 
 while True:
-    print("Exepmplos de frases em Portugues: /n")
-    print("Aplicar 1 dose de benzetacil 20ml de 8 em 8 horas")
-    print("Tomar 1 comprimido de paracetamol 50mg de 12 em 12 horas")
-    print("Tomar 2 comprimidos de dipirona 50mg de 8 em 8 horas")
-    print("Exepmplos de frases em Inglês: /n")
-    print("Take 1 pill in paracetamol 50mg every 12 hours")
-    print("Apply 1 injection in profenid 10ml every 12 hours")
-   
+      
     name = input("Digite a frase ou digite 's' para sair: ")
 
     if name.lower() == 's':
@@ -22,9 +27,10 @@ while True:
      # Verifica se a frase contém palavras em inglês
     if any(word in name.lower() for word in ["take", "pill", "injection"]):
         try:
-            i = eng.parse(name)
+            i = por.parse(name)
             p, e = i.__next__()
-            print("Prescrição Válida: " + eng.linearize(e))
+            print("Prescrição Válida: " + por.linearize(e))
+            break
         except pgf.ParseError:
             print("Prescrição Inválida, tente novamente")
     else:
@@ -32,5 +38,6 @@ while True:
             i = por.parse(name)
             p, e = i.__next__()
             print("Prescrição Válida: " + por.linearize(e))
+            break
         except pgf.ParseError:
             print("Prescrição Inválida, tente novamente")
